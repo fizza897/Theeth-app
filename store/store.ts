a84bb605bc14bf63e50a175dc18cf46c865015c7
fizza897 <fizzaazam6@gmail.com>
2025-04-02 20:17:14 +0500

    done

--- store/store.ts
+++ store/store.ts
@@ -12,7 +12,21 @@ const persistUserReducer =persistReducer(userPersistedConfig,userReducer)
 
 export const store= configureStore({
     reducer:{
-        [api.reducerPath]:api.reducer,
+        [api.reducerPath]:api.reducer, // rtk query api
         user:persistUserReducer
-    }
-})
+    },
+    middleware:(getDefaultMiddleWare)=>
+        getDefaultMiddleWare({
+            serializableCheck:{
+                ignoredActions:[FLUSH,REHYDRATE,PAUSE,PURGE,PERSIST,REGISTER]
+            },
+        }).concat(api.middleware)
+});
+// setup the listner  for RTK QUERY
+setupListeners(store.dispatch);
+// create a persistor
+export const persistor =persistStore(store)
+// infer the `RootState` and `Appdispatch` types from the store it self
+export type RootState = ReturnType<typeof store.getState>
+// interred type:{postState,comments:CommentState,users:UserState}
+export type AppDispatch =typeof store.dispatch
