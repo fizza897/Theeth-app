98e1faba24160a7568f72f786910cc4a0114817d
fizza897 <fizzaazam6@gmail.com>
2025-04-02 17:45:17 +0500

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/Header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
+  display: "swap"
 });
 
 export const metadata: Metadata = {
@@ -29,7 +26,7 @@ export default function RootLayout({
       <Header/>
       
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={roboto_mono.className}
         >
         {children}
       </body>
